9cc8f0fc994409314883dd557933b7f96a07702f
Michael Toth <mtoth003@gmail.com>
2021-10-18 14:07:54 -0400

    commit tree 0dcae0a4b6aedbeb7a084a15f61f792ea0b7f700

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+}
+
+
+function fuzzyMatch(drivers, providedName){
+    return drivers.filter(driver => driver.toLowerCase().indexOf(providedName.toLowerCase()) === 0)
+}
+
+function matchName(providedObject, enteredName){
+    return providedObject.filter(object => object.name === enteredName)
+}
